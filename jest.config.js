0a24e30f4e27b01b887dcdf8969b0da4c5994828
Ian Dela Cruz <iandc76@gmail.com>
2020-10-20 10:49:22 +0800

    Switch to rollup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
   collectCoverage: true,
   coverageReporters: ['text', 'lcov'],
   collectCoverageFrom: ['src/**/*.ts'],
+  transformIgnorePatterns: ['./node_modules/'],
   testResultsProcessor: 'jest-sonar-reporter',
 };
